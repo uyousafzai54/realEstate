d74567470fa4ef4d776f6480bb342fc124d18027
Rahul Aggarwal <rahulaggarwal1016@gmail.com>
2021-01-20 11:06:34 -0500

    initial backend setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-// backend
+// primary backend file
+const { ApolloServer } = require('apollo-server');
+
+// import resolvers
+// import typeDefs
+
+const PORT = process.env.PORT || 5000;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen({ port: PORT }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});

7fccbeebe38b69e4d554f16b2b07f5a1aa0ef7dd
Rahul Aggarwal <rahulaggarwal1016@gmail.com>
2021-02-14 18:14:39 -0500

    added .env client side

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,8 @@
-import Head from 'next/head';
-import auth0 from './api/utils/auth0'; 
-export default function Home({user}) {
+import Head from "next/head";
+import auth0 from "./api/utils/auth0";
+import Link from "next/link";
+
+export default function Home({ user }) {
   return (
     <>
       <Head>
@@ -9,33 +11,33 @@ export default function Home({user}) {
       </Head>
 
       <main>
-                <nav>
-                    <div className="flex items-center justify-between py-4  ">
-                        <div className="flex justify-between items-center">
-                            <div className="text-2xl font-bold text-gray-800 md:text-3xl">
-                                <a href="/randomPage">Random Page</a>
-                            </div>
-                        </div>
-                        <div className="flex">
-                            {user ? (
-                                <a
-                                    href="/api/logout"
-                                    className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
-                                >
-                                    Logout
-                                </a>
-                            ) : (
-                                <a
-                                    href="/api/login"
-                                    className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
-                                >
-                                    Login
-                                </a>
-                            )}
-                        </div>
-                    </div>
-                </nav>
-            </main>
+        <nav>
+          <div className="flex items-center justify-between py-4  ">
+            <div className="flex justify-between items-center">
+              <div className="text-2xl font-bold text-gray-800 md:text-3xl">
+                <a href="/randomPage">Random Page</a>
+              </div>
+            </div>
+            <div className="flex">
+              {user ? (
+                <Link
+                  href="/api/logout"
+                  className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
+                >
+                  <a>Logout</a>
+                </Link>
+              ) : (
+                <Link
+                  href="/api/login"
+                  className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
+                >
+                  <a>Login</a>
+                </Link>
+              )}
+            </div>
+          </div>
+        </nav>
+      </main>
       <div>
         <h1>Hi There! Welcome to the Home Page</h1>
       </div>
@@ -43,12 +45,11 @@ export default function Home({user}) {
   );
 }
 
-export async function getServerSideProps(context)
-{
-  const session = await auth0.getSession(context.req); 
+export async function getServerSideProps(context) {
+  const session = await auth0.getSession(context.req);
   return {
     props: {
-      user: session?.user || null, 
-    }
-  }
+      user: session?.user || null,
+    },
+  };
 }
